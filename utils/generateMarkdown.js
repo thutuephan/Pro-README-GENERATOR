5324a3165ed54fcd3e0c2ee84d1138adfc72d50b
Thu Phan <alicebe.phan@gmail.com>
2021-10-06 14:36:17 -0500

    Fix typo error in index.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,36 +1,35 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-// Can I do switch instead of if-else?// switch(asnwers.license)...?
-function renderLicenseBadge(license) {
-  if (license){
-  `![License Badge](https://img.shields.io/badge/license-${license}-yellow.svg)`
-} else ('');
+// // TODO: Create a function that returns a license badge based on which license is passed in
+// // If there is no license, return an empty string
+// // Can I do switch instead of if-else?// switch(asnwers.license)...?
+// function renderLicenseBadge(license) {
+//   if (license){
+//   `![License Badge](https://img.shields.io/badge/license-${license}-yellow.svg)`
+// } else ('');
 
-};
-return renderLicenseBadge(license);
+// };
+// return renderLicenseBadge(license);
 
 
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license === 'Apache 2.0') {
-  `http://www.apache.org/licenses/LICENSE-2.0`
-  }
-  else if (license === 'MIT') {
-    `https://mit-license.org`
-  } 
-  else if (license === 'GPL 3.0'){
-    `http://www.gnu.org/licenses/`
-  }
-  
-
+// // TODO: Create a function that returns the license link
+// // If there is no license, return an empty string
+// function renderLicenseLink(license) {
+//   if (license === 'Apache 2.0') {
+//   `http://www.apache.org/licenses/LICENSE-2.0`
+//   }
+//   else if (license === 'MIT') {
+//     `https://mit-license.org`
+//   } 
+//   else  (license === 'GPL 3.0'){
+//     `http://www.gnu.org/licenses/`
+//   }
+// };
 
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  //This project is licensed under the ${license} license.
+  `## License - This project is licensed under the ${license} license.`
 }
 
 // TODO: Create a function to generate markdown for README
